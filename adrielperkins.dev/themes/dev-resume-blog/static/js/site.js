30663f624b52f2e19b30d7ba8a486196ad7d0f73
Adriel Perkins <adriel@Mac-mini.local>
2020-07-19 23:57:05 -0400

    initial commit

---
+++ adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
@@ -0,0 +1,30 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// navbar menu
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    smartBackspace: true,
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1000,
+    showCursor: false
+});
+
+
+
+//smooth scroll for anchor
