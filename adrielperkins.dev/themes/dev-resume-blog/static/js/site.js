66145d3ff187e722b7568d5a4d62f0b78501fb02
Adriel Perkins <adriel@adrielperkins.com>
2020-10-17 12:28:51 -0400

    Adding css menu instead of js trigger

--- adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
+++ adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
@@ -2,17 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // navbar menu
-$(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
-});
+//$(document).ready(function() {
+//
+//  // Check for click events on the navbar burger icon
+//  $(".navbar-burger").click(function() {
+//
+//      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//      $(".navbar-burger").toggleClass("is-active");
+//      $(".navbar-menu").toggleClass("is-active");
+//
+//  });
+//});
 
 
 var typed = new Typed('#typed', {
