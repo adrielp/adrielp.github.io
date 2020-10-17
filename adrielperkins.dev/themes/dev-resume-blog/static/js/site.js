abc532298db5c52c17c4355ddf0ed528c8552f69
Adriel Perkins <adriel@adrielperkins.com>
2020-10-17 14:46:47 -0400

    Re-added jquery menu toggle

--- adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
+++ adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
@@ -1,3 +1,15 @@
+// navbar menu
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+       $(".navbar-burger").toggleClass("is-active");
+       $(".navbar-menu").toggleClass("is-active");
+
+    });
+});
+
 var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     smartBackspace: true,
