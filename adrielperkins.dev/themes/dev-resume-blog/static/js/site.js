aa84024f906fa98945dc78b3a167d2cb1b6f0100
Adriel Perkins <adriel@adrielperkins.com>
2020-10-17 12:34:41 -0400

    Removed JS, added CSS options to Projects menus

--- adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
+++ adrielperkins.dev/themes/dev-resume-blog/static/js/site.js
@@ -1,20 +1,3 @@
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// navbar menu
-//$(document).ready(function() {
-//
-//  // Check for click events on the navbar burger icon
-//  $(".navbar-burger").click(function() {
-//
-//      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-//      $(".navbar-burger").toggleClass("is-active");
-//      $(".navbar-menu").toggleClass("is-active");
-//
-//  });
-//});
-
-
 var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     smartBackspace: true,
